9b3fc12a3afbab0820c4fc112edca8725db21a91
Gonzei <hawkeyejal@gmail.com>
2024-04-10 19:45:53 -0400

    button ids

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -64,10 +64,10 @@
 	});
 </script>
 
-<button on:click={signInWithTwitch}>Sign in with Twitch</button>
+<button id = "SignInButton" on:click={signInWithTwitch}>Sign in with Twitch</button>
 
 <form action="?/signOut" method="POST" use:enhance>
-	<button type="submit">Sign out</button>
+	<button id = "SignOutButton" type="submit">Sign out</button>
 </form>
 
 <img src={data.session?.user.user_metadata.avatar_url} alt="avatar" />
