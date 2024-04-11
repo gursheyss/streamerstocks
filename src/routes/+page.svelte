920408b20b1f177b38772d7006a6457111278ad6
gursheyss <rkwarya@gmail.com>
2024-04-11 03:45:44 -0700

    remove old table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -167,39 +167,3 @@
 <!-- PLACEHOLDER VALUES FOR NOW -->
 <button id="BuyButton" on:click={() => updateStockAndBal(-3, placeHolderID)}>Buy</button>
 <button id="SellButton" on:click={() => updateStockAndBal(3, placeHolderID)}>Sell</button>
-
-<div>bal: ${bal}</div>
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-	{#each marketData as item}
-		<div class="bg-white shadow-md rounded-lg p-4">
-			<div class="flex items-center mb-4">
-				{#if item.image}
-					<img src={item.image} alt={item.name} class="w-10 h-10 rounded-full mr-4" />
-				{:else}
-					<div class="w-10 h-10 bg-gray-300 rounded-full mr-4"></div>
-				{/if}
-				<div>
-					<h2 class="text-lg font-semibold">{item.name}</h2>
-					<p class="text-gray-500">${item.ticker}</p>
-				</div>
-			</div>
-			<div class="flex justify-between items-center">
-				<p class="text-2xl font-bold">${item.price}</p>
-				<div>
-					{#if item.low}
-						<p class="text-gray-500">Low: ${item.low}</p>
-					{/if}
-					{#if item.high}
-						<p class="text-gray-500">High: ${item.high}</p>
-					{/if}
-				</div>
-			</div>
-			{#if item.market_cap}
-				<p class="text-gray-500 mt-2">Market Cap: ${item.market_cap}</p>
-			{/if}
-			{#if item.volume}
-				<p class="text-gray-500 mt-1">Market Volume: ${item.volume}</p>
-			{/if}
-		</div>
-	{/each}
-</div>
