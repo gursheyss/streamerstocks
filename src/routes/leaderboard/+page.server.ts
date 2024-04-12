5686c94bda9499211dcd36933937b4c71fc2b452
Ellahi <ellahinator02@gmail.com>
2024-04-12 18:05:30 -0400

    added sql function as a comment

--- src/routes/leaderboard/+page.server.ts
+++ src/routes/leaderboard/+page.server.ts
@@ -7,3 +7,44 @@ export const load = async ({ locals }) => {
 
 	return { marketData: data };
 };
+
+/*
+
+SQL function calculate_pnl_with_networth():
+
+DROP FUNCTION IF EXISTS calculate_pnl_with_networth;
+
+CREATE OR REPLACE FUNCTION calculate_pnl_with_networth()
+RETURNS TABLE (
+  user_id uuid,
+  username text,
+  avatar_url text,
+  pnl numeric,
+  net_worth numeric,
+  trade_count bigint
+) AS $$
+BEGIN
+  RETURN QUERY
+    SELECT
+      p.id AS user_id,
+      p.username,
+      p.avatar_url,
+      COALESCE(SUM((CASE 
+                        WHEN t.status = 'sold' THEN t.sale_volume * CAST(t.sold_price AS numeric)
+                        ELSE CAST(m.price AS numeric) * t.purchase_volume
+                    END) - (CAST(t.bought_price AS numeric) * t.purchase_volume)), 0) AS pnl,
+      CAST(p.balance AS numeric) + COALESCE(SUM((CASE 
+                                                    WHEN t.status = 'sold' THEN t.sale_volume * CAST(t.sold_price AS numeric)
+                                                    ELSE CAST(m.price AS numeric) * t.purchase_volume
+                                                END)), 0) AS net_worth,
+      COUNT(t."trade id") AS trade_count
+    FROM profiles p
+    LEFT JOIN trades t ON t.user_id = p.id AND (t.status = 'bought' OR t.status = 'sold')
+    LEFT JOIN market m ON m.id = t.stock_id
+    GROUP BY p.id
+    HAVING COUNT(t."trade id") > 0
+    ORDER BY net_worth DESC;
+END; $$ LANGUAGE plpgsql;
+
+
+*/
