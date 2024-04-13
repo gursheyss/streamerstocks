9b871af1a21637d17fa7631532f2de17e7ef32ca
gursheyss <rkwarya@gmail.com>
2024-04-13 14:08:44 -0700

    disable button until request completes

--- src/lib/components/BuyandSell.svelte
+++ src/lib/components/BuyandSell.svelte
@@ -9,17 +9,19 @@
 		ticker
 	}: { uuid: string; stockID: number; currentPrice: number; userBalance: number; ticker: string } =
 		$props();
-	let amount: string = $state('');
+	let amount = $state('');
+	let loading = $state(false);
 
 	async function updateStockAndBal(uuid: string, stockID: number, amt: number) {
+		loading = true;
 		const response = await fetch('/api/trade', {
 			method: 'POST',
 			body: JSON.stringify({ uuid, amt, stockID }),
-			headers: {
-				'Content-Type': 'application/json'
-			}
+			headers: { 'Content-Type': 'application/json' }
 		});
 		const resp = await response.json();
+		loading = false;
+
 		if (resp['success'] == false) {
 			toast.error('Please ensure that you have enough money to purchase or shares to sell.');
 		} else if (amt < 0) {
@@ -47,6 +49,7 @@
 		<button
 			class="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-lime-200 dark:focus:ring-lime-800"
 			on:click={() => updateStockAndBal(uuid, stockID, -Number(amount))}
+			disabled={loading}
 		>
 			<span
 				class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
@@ -57,6 +60,7 @@
 		<button
 			class="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800"
 			on:click={() => updateStockAndBal(uuid, stockID, Number(amount))}
+			disabled={loading}
 		>
 			<span
 				class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0"
