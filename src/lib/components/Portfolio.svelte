efd6e1cae94df2236cd7170e2c32731dcd9e55ee
bobo <danknessdra@gmail.com>
2024-04-12 17:53:40 -0700

    renamed buyingpower to balance and total account worth to networth

--- src/lib/components/Portfolio.svelte
+++ src/lib/components/Portfolio.svelte
@@ -15,8 +15,8 @@
 <div class="bg-gray shadow-lg rounded-lg p-6">
 	<div class="flex items-center justify-between">
 		<div>
-			<h3 class="text-2xl font-bold font-inter">Total Account Value: ${netWorth.toFixed(2)}</h3>
-			<p>Buying Power: ${balance.toFixed(2)}</p>
+			<h3 class="text-2xl font-bold font-inter">Balance: ${balance.toFixed(2)}</h3>
+			<p>Networth: ${netWorth.toFixed(2)}</p>
 		</div>
 	</div>
 </div>
