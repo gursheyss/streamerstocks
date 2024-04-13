495acdce929d28d238b580e2964f2c9f9caf1186
gursheyss <rkwarya@gmail.com>
2024-04-13 05:24:55 -0700

    clean up logic

--- src/routes/stock/[ticker]/+page.svelte
+++ src/routes/stock/[ticker]/+page.svelte
@@ -22,19 +22,13 @@
 	let inventoryData = $state<InventoryItem[] | null>(data.userInventory);
 
 	function calcNW(x: InventoryItem[]): number {
-		let snapshotBalance = 0;
-		if (userBalance != null) {
-			snapshotBalance = userBalance;
-		}
-		let total = 0;
-		if (snapshotBalance != 0) {
-			total = snapshotBalance;
-		}
+		let total = userBalance || 0;
 		x.forEach((element) => {
 			total += element.quantity * element.market.price;
 		});
 		return total;
 	}
+
 	$effect(() => {
 		const marketSubscription = supabase
 			.channel('market')
