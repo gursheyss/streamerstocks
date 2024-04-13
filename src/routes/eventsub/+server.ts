d617dbb26698a71bd649cff3e1707b45ad5ff810
NicholasLe04 <Nicholasl.le224@gmail.com>
2024-04-13 15:07:00 -0700

    Update amount_redeemed column + comments

--- src/routes/eventsub/+server.ts
+++ src/routes/eventsub/+server.ts
@@ -14,16 +14,19 @@ const HMAC_PREFIX = 'sha256=';
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
-
+    // Add the amount of channel points to the user's balance
     const addChannelPointsToBalance = async (amt_weeniebucks:number, user_id:string) => {
-        console.log("Attempting to add " + amt_weeniebucks + " weeniebucks to user " + user_id)
-        const { data: userData, error: userError } = await supabase.from('profiles').select('balance').eq('user_id', user_id);
-        if (userError || userData === undefined) console.error("User does not have a bopstock account.");
+        const { data: userData, error: userError } = await supabase.from('profiles').select('balance, amount_redeemed',).eq('user_id', user_id);
+        if (userError || userData === undefined) {
+            console.error("User not found");
+        }
         else {
             userData[0]['balance'] += amt_weeniebucks;
-            console.log(userData)
+            userData[0]['amount_redeemed'] += amt_weeniebucks;
             const { data: updateData, error: updateError } = await supabase.from('profiles').update(userData[0]).eq('user_id', user_id).select();
-            if (updateError) console.error("Failed to update user balance.");
+            if (updateError) {
+                console.error("Failed to update balance");
+            };
         }
     }
 
@@ -31,19 +34,20 @@ export async function POST({ request }) {
     const message = await getHmacMessage(request);
     const hmac = HMAC_PREFIX + getHmac(secret, message);
 
+    // Verify the message
     if (await verifyMessage(hmac, request.headers.get(TWITCH_MESSAGE_SIGNATURE) as string)) {
-        console.log("signature match");
-
         const notification = await request.json();
+        // Handle the notification
         if (request.headers.get(MESSAGE_TYPE) === 'notification') {
             if (
                 notification.subscription.type === 'channel.channel_points_custom_reward_redemption.add'
-                // && notification.event.reward.id === 'THE ID FOR THE REWARD WE LISTENING FOR'
+                // && notification.event.reward.id === 'ASK DANISH FOR THE ID FOR THE REWARD WE LISTENING FOR'
             ) {
                 addChannelPointsToBalance(notification.event.reward.cost/10, notification.event.user_id);
             }
             return new Response(null, {status: 204});
         }
+        // Handle the challenge
         else if (request.headers.get(MESSAGE_TYPE) === 'webhook_callback_verification') {
             return new Response(notification.challenge, {
                 status: 200,
@@ -52,17 +56,20 @@ export async function POST({ request }) {
                 }
             });
         }
+        // Handle the revocation
         else if (request.headers.get(MESSAGE_TYPE) === 'revocation') {
             console.log(`${notification.subscription.type} has been revoked`);
             console.log(`Reason: ${notification.subscription.status}`);
             console.log(`condition: ${JSON.stringify(notification.subscription.condition)}`);
             return new Response(null, {status: 204});
         }
+        // Handle unknown message types
         else {
             console.log(`Unknown message type: ${request.headers.get(MESSAGE_TYPE)}`);
             return new Response(null, {status: 204});
         }
     }
+    // If the message is not verified, return a 403
     else {
         console.log("403");
         return new Response(null, {status: 403});
@@ -70,7 +77,6 @@ export async function POST({ request }) {
 }
 
 function getSecret() : string {
-    console.log("EVENT SUB SECRET: " + PUBLIC_TWITCH_EVENTSUB_SECRET);
     return PUBLIC_TWITCH_EVENTSUB_SECRET as string;
 }
 
@@ -94,7 +100,5 @@ async function verifyMessage(hmac: string, verifySignature: string | null): Prom
     if (verifySignature === null) {
         return false;
     }
-    console.log("hmac: " + hmac);
-    console.log("verifySignature: " + verifySignature);
     return crypto.timingSafeEqual(Buffer.from(hmac), Buffer.from(verifySignature));
 }
