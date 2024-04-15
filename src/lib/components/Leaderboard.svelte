15fa1ce5e6e4bc1f3ad7316868caa9e51bef1e7b
gursheyss <rkwarya@gmail.com>
2024-04-15 11:08:18 -0700

    fix mobile ui

--- src/lib/components/Leaderboard.svelte
+++ src/lib/components/Leaderboard.svelte
@@ -47,68 +47,79 @@
 	<div class="flex mt-16 justify-center h-screen">Fetching data...</div>
 {:else}
 	<div class="bg-gray2 rounded-lg shadow-lg p-4 font-inter">
-		<div class="flex justify-center items-center mb-2 m-2 mt-4">
+		<div class="flex flex-col items-center mb-4">
 			<h2 class="text-xl font-bold text-white">Leaderboard</h2>
+			<p class="text-xs text-gray-400 italic">Updates every 5 minutes</p>
 		</div>
-		<p class=" text-xs text-center text-gray-400 italic">Updates every 5 minutes</p>
-		<div class="flex m-2">
-			<table class="w-full border-b border-gray-700">
-				<thead>
-					<tr class="text-white font-bold">
-						<th class="text-left py-2 cursor-pointer" on:click={() => sortData('rank')}>
-							Rank {@html getSortIcon()}</th
-						>
-						<th class="text-left py-2">User</th>
-						<th class="text-left py-2 cursor-pointer" on:click={() => sortData('trade_count')}>
-							# of Trades {@html getSortIcon()}
-						</th>
-						<th class="text-left py-2 cursor-pointer" on:click={() => sortData('pnl')}>
-							PnL {@html getSortIcon()}
-						</th>
-						<th class="text-left py-2 cursor-pointer" on:click={() => sortData('net_worth')}>
-							Networth {@html getSortIcon()}
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					{#each leaderboardData.slice(0, numRows) as leaderboardItem, index}
-						<tr
-							class="text-white font-bold items-center hover:bg-lightgray cursor-pointer"
-							on:click={() => handleRowClick(leaderboardItem.username)}
-						>
-							<td class="py-4 items-center">
-								<span>{leaderboardItem.rank}</span>
-							</td>
-							<td class="py-4 flex items-center">
-								<img
-									src={leaderboardItem.avatar_url}
-									alt="avatar"
-									class="w-8 h-8 rounded-full mr-2"
-								/>
-								<span>{leaderboardItem.username}</span>
-							</td>
-							<td class="py-4 items-center">
-								<span>{leaderboardItem.trade_count}</span>
-							</td>
-							<td class="py-4 items-center">
-								<span
-									class={leaderboardItem.pnl > 0
-										? 'text-green-400'
-										: leaderboardItem.pnl < 0
-											? 'text-red-400'
-											: 'text-gray-400'}
+		<div class="overflow-x-auto">
+			<div class="inline-block min-w-full">
+				<div class="overflow-hidden">
+					<table class="min-w-full divide-y divide-gray-700">
+						<thead>
+							<tr class="text-white font-bold">
+								<th class="px-6 py-3 text-left cursor-pointer" on:click={() => sortData('rank')}>
+									Rank {@html getSortIcon()}
+								</th>
+								<th class="px-6 py-3 text-left">User</th>
+								<th
+									class="px-6 py-3 text-left cursor-pointer"
+									on:click={() => sortData('trade_count')}
 								>
-									{leaderboardItem.pnl >= 0 ? '+' : ''}
-									${leaderboardItem.pnl?.toFixed(2)}
-								</span>
-							</td>
-							<td class="py-4 items-center">
-								<span class="text-white">${leaderboardItem.net_worth.toFixed(2)}</span>
-							</td>
-						</tr>
-					{/each}
-				</tbody>
-			</table>
+									# of Trades {@html getSortIcon()}
+								</th>
+								<th class="px-6 py-3 text-left cursor-pointer" on:click={() => sortData('pnl')}>
+									PnL {@html getSortIcon()}
+								</th>
+								<th
+									class="px-6 py-3 text-left cursor-pointer"
+									on:click={() => sortData('net_worth')}
+								>
+									Networth {@html getSortIcon()}
+								</th>
+							</tr>
+						</thead>
+						<tbody class="divide-y divide-gray-700">
+							{#each leaderboardData.slice(0, numRows) as leaderboardItem, index}
+								<tr
+									class="text-white font-bold hover:bg-lightgray cursor-pointer"
+									on:click={() => handleRowClick(leaderboardItem.username)}
+								>
+									<td class="px-6 py-4 whitespace-nowrap">
+										<span>{leaderboardItem.rank}</span>
+									</td>
+									<td class="px-6 py-4 whitespace-nowrap">
+										<div class="flex items-center">
+											<img
+												src={leaderboardItem.avatar_url}
+												alt="avatar"
+												class="w-8 h-8 rounded-full mr-2"
+											/>
+											<span>{leaderboardItem.username}</span>
+										</div>
+									</td>
+									<td class="px-6 py-4 whitespace-nowrap">
+										<span>{leaderboardItem.trade_count}</span>
+									</td>
+									<td class="px-6 py-4 whitespace-nowrap">
+										<span
+											class={leaderboardItem.pnl > 0
+												? 'text-green-400'
+												: leaderboardItem.pnl < 0
+													? 'text-red-400'
+													: 'text-gray-400'}
+										>
+											{leaderboardItem.pnl >= 0 ? '+' : ''}${leaderboardItem.pnl?.toFixed(2)}
+										</span>
+									</td>
+									<td class="px-6 py-4 whitespace-nowrap">
+										<span class="text-white">${leaderboardItem.net_worth.toFixed(2)}</span>
+									</td>
+								</tr>
+							{/each}
+						</tbody>
+					</table>
+				</div>
+			</div>
 		</div>
 		{#if numRows < leaderboardData.length}
 			<div class="flex justify-center mt-4">
