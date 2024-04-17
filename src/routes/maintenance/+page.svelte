2af9f9487ca2cd3dadeec6e6df09cee699c52481
gursheyss <rkwarya@gmail.com>
2024-04-16 22:27:12 -0700

    add discord to maintenance page

--- src/routes/maintenance/+page.svelte
+++ src/routes/maintenance/+page.svelte
@@ -1,5 +1,10 @@
 <div class="flex flex-col items-center justify-center h-screen space-y-4">
-	<div class="font-bold">we r down for maintenance lol</div>
+	<div class="font-bold">
+		we r down for maintenance lol, join our <a
+			class="text-pink-300 underline"
+			href="https://discord.com/invite/2asZArVb7s">discord</a
+		> to know when we're back
+	</div>
 	<div class="text-center">
 		<div>postmortems</div>
 		<div>4/16/24 10:05PM PST the mira market crash</div>
