b78438f19c8bd40ded02619acc03106a738dfd7f
gursheyss <rkwarya@gmail.com>
2024-04-16 18:53:37 -0700

    add comment validation again

--- src/lib/components/Comments.svelte
+++ src/lib/components/Comments.svelte
@@ -3,15 +3,35 @@
 	import { formatDistanceToNow, parseISO } from 'date-fns';
 
 	import type { Comment } from '$lib/types';
+	import { toast } from 'svelte-sonner';
 
 	let { comments, currentId }: { comments: Comment[]; currentId: string } = $props();
 	let newComment = $state('');
+
+	let isValid = $state(false);
+	$effect(() => {
+		isValid = /\w/.test(newComment);
+	});
 </script>
 
 <div class="bg-gray2 sm:p-6 rounded-lg shadow-md font-inter text-white">
 	<h2 class="text-2xl font-bold mb-4">Comments</h2>
 
-	<form action="?/submitComment" method="POST" class="mb-6 flex" use:enhance>
+	<form
+		action="?/submitComment"
+		method="POST"
+		class="mb-6 flex"
+		use:enhance={() => {
+			return async ({ result }) => {
+				if (result.type === 'success') {
+					toast.success('Comment submitted successfully');
+				}
+				if (result.type === 'failure') {
+					toast.error('You must be logged in to comment');
+				}
+			};
+		}}
+	>
 		<textarea
 			name="comment"
 			class="w-full border border-gray-600 bg-lightgray rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:lightgray mr-2"
@@ -24,6 +44,7 @@
 		<button
 			type="submit"
 			class="bg-lightgray text-white py-2 px-4 rounded-md transition-colors duration-300"
+			disabled={!isValid}
 		>
 			Submit
 		</button>
