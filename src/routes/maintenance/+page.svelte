483b30779e56e8f7d3b3c5cc33007a5f88f676d7
gursheyss <rkwarya@gmail.com>
2024-04-16 22:19:25 -0700

    Refactor maintenance page layout and add postmortem information

--- src/routes/maintenance/+page.svelte
+++ src/routes/maintenance/+page.svelte
@@ -1,1 +1,7 @@
-<div class="flex items-center justify-center h-screen font-bold">we r down for maintenance lol</div>
+<div class="flex flex-col items-center justify-center h-screen space-y-4">
+	<div class="font-bold">we r down for maintenance lol</div>
+	<div class="text-center">
+		<div>postmortems</div>
+		<div>4/16/24 10:05PM PST the mira market crash</div>
+	</div>
+</div>
