95bdfbd9161de1f9afc8f91b476d228a2eece976
Ellahi <ellahinator02@gmail.com>
2024-04-20 15:05:52 -0400

    fix text color

--- src/routes/stock/[ticker]/+page.svelte
+++ src/routes/stock/[ticker]/+page.svelte
@@ -255,7 +255,7 @@
 								1H
 							</button>
 							<button
-								class={` px-2 py-2 rounded-lg${
+								class={` px-2 py-2 rounded-lg ${
 									selectedDateRange === '12 hour' ? 'text-white' : 'text-gray-500'
 								}`}
 								onclick={() => (selectedDateRange = '12 hour')}
