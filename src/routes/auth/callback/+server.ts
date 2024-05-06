3a3c911d88be50b601ab6d0a9a390b0cce5442f3
Ellahi <ellahinator02@gmail.com>
2024-05-06 18:19:54 -0400

    encodeuri

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -11,7 +11,7 @@ export const GET = async (event) => {
 	if (code) {
 		const { error } = await supabase.auth.exchangeCodeForSession(code);
 		if (!error) {
-			redirect(303, `/${next.slice(1)}?signedIn=True`);
+			redirect(303, `/${encodeURIComponent(next.slice(1))}?signedIn=True`);
 		}
 	}
 
