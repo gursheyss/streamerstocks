f0f3ef159f388264e5c3ac853b6400aaf8770ffa
gursheyss <rkwarya@gmail.com>
2024-05-09 19:04:10 -0700

    remove old text

--- src/routes/maintenance/+page.svelte
+++ src/routes/maintenance/+page.svelte
@@ -1,10 +1,7 @@
 <div class="flex flex-col items-center justify-center h-screen space-y-4">
 	<div class="font-bold">
-		<a
-			class="text-pink-300 underline"
-			href="https://discord.com/invite/2asZArVb7s">discord</a
-		> <br>
-		this website is meant to be a meme and community project. fuck those who are taking this seriously. go touch grass.
+		<a class="text-pink-300 underline" href="https://discord.com/invite/2asZArVb7s">discord</a>
+		<br />
 	</div>
 	<div class="text-center">
 		<div>postmortems</div>
